ae56ba4f5fc06574d7c2ae1a5794791b0bd788c8
Caio Paiva <caiowick@hotmail.com>
2021-04-11 21:53:48 -0300

    Métodos de lista

--- index.php
+++ index.php
@@ -2,16 +2,36 @@
 
     require_once("config.php");
 
-    // $sql = new Sql();
+    /*
+    $sql = new Sql();
+    $usuarios = $sql->select("SELECT * FROM tb_usuarios");
+    echo json_encode($usuarios); 
+    */
 
-    // $usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
-    // echo json_encode($usuarios);
-
+    //Carrega um usuário
+    /*
     $root = new Usuario();
-
     $root->loadbyId(3);
+    echo $root; 
+    */
 
-    echo $root;
+    //Carrega uma lista de usuários
+    // $lista = Usuario::getList();
+    
 
+    // Carrega uma lista de usuaários buscando pelo login
+    /*
+    $search = Usuario::search("ca");
+    echo json_encode($search ); 
+    */
+
+    //Carrega um usuário usando login e senha
+    $usuario = new Usuario();
+    $usuario->login("joão", "1122"); 
+    echo $usuario;
+    
+
+    
+
+     
 ?>
